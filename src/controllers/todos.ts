2272c6363f398bbc64f1397d37c359b07e4f4c0e
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-25 17:03:39 -0500

    Connecting create to format of new items

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -6,4 +6,6 @@ const TODOS: Todo[] = [];
 export const createTodo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
   const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
 };
